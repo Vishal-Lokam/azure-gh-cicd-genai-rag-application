940a7314d749dfc9a13e0375ca8969ac015e1068
Vishal Lokam <55016087+VishalLokam@users.noreply.github.com>
2025-02-27 00:23:40 +0530

    testing jq querying value (#14)
    Removed .tfvars.json files from gitignore. Changed terrform plan

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_resource_group" "infrastructure_rg" {
   name     = var.resource_group_name
-  location = "westindia"
+  location = var.location
 }
 
 data "azurerm_client_config" "current" {
