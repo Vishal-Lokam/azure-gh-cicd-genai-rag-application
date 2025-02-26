2b345be7e19ae1726848d14d6c6908b94f68c104
Vishal Lokam <55016087+VishalLokam@users.noreply.github.com>
2025-02-26 22:12:36 +0530

    Creating basic infrastructure. Implementing CICD pipeline to run this basic infra.
    Creating basic infrastructure. Implementing CICD pipeline to run this basic infra.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,18 @@ variable "subscription_id" {
   description = "Subscription id of the Azure subscription"
   type        = string
 }
+
+variable "keyvault_name" {
+  description = "Key valut name"
+  type        = string
+}
+
+variable "location" {
+  description = "Location for prerequisite resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group for the infrastructure"
+  type        = string
+}
