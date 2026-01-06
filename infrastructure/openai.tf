763ea86e8ded07483ebb7fd3ac8382523753f8fa
VishalLokam <vishalvikaslokam@gmail.com>
2026-01-06 23:47:52 +0530

    fix errors

--- infrastructure/openai.tf
+++ infrastructure/openai.tf
@@ -1,7 +1,7 @@
 resource "azurerm_cognitive_account" "azure_openai_account" {
   name                = var.azure_openai_account_name
   location            = "swedencentral"
-  resource_group_name = azurerm_resource_group.infrastructure_rg
+  resource_group_name = azurerm_resource_group.infrastructure_rg.name
   kind                = "OpenAI"
   sku_name            = "S0"
 
