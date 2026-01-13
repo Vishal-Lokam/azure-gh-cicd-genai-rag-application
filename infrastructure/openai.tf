ac266aee2ff7e670ab9c128926f81b01922411d1
VishalLokam <vishalvikaslokam@gmail.com>
2026-01-13 20:28:04 +0530

    add model deployment

--- infrastructure/openai.tf
+++ infrastructure/openai.tf
@@ -1,6 +1,6 @@
 resource "azurerm_cognitive_account" "azure_openai_account" {
   name                = var.azure_openai_account_name
-  location            = "swedencentral"
+  location            = "southindia"
   resource_group_name = azurerm_resource_group.infrastructure_rg.name
   kind                = "OpenAI"
   sku_name            = "S0"
@@ -9,3 +9,34 @@ resource "azurerm_cognitive_account" "azure_openai_account" {
 }
 
 # TODO: Add Azure OpenAI deployment here
+resource "azurerm_cognitive_deployment" "astral_gpt_41_nano" {
+  name                 = "gpt-4.1-nano"
+  cognitive_account_id = azurerm_cognitive_account.azure_openai_account.id
+
+  model {
+    format  = "OpenAI"
+    name    = "gpt-4.1-nano"
+    version = "2025-04-14"
+  }
+
+  sku {
+    name     = "GlobalStandard"
+    capacity = 10
+  }
+}
+
+resource "azurerm_cognitive_deployment" "astral_text_embedding_large_3" {
+  name                 = "text-embedding-3-large"
+  cognitive_account_id = azurerm_cognitive_account.azure_openai_account.id
+
+  model {
+    format  = "OpenAI"
+    name    = "text-embedding-3-large"
+    version = 1
+  }
+
+  sku {
+    name     = "Standard"
+    capacity = 10
+  }
+}
