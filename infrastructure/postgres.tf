372325640937811857726f602208b536982c7023
VishalLokam <vishalvikaslokam@gmail.com>
2026-01-14 21:53:08 +0530

    fix postgres issue

--- infrastructure/postgres.tf
+++ infrastructure/postgres.tf
@@ -11,14 +11,14 @@ data "azurerm_key_vault_secret" "postgres_admin_password" {
 
 
 resource "azurerm_postgresql_flexible_server" "astralbound_postgres" {
-  name                          = "astralbound_postgres"
+  name                          = "astralbound-postgres"
   resource_group_name           = azurerm_resource_group.infrastructure_rg.name
   location                      = var.location
   version                       = "17"
   public_network_access_enabled = true
   administrator_login           = var.postgres_admin_login
   administrator_password        = data.azurerm_key_vault_secret.postgres_admin_password.value
-  storage_mb                    = "32786"
+  storage_mb                    = "32768"
   sku_name                      = "B_Standard_B1ms"
 
   tags = local.tags
