f1a4ed7deb41d27cbb05fcd043aa8166468be7ef
VishalLokam <vishalvikaslokam@gmail.com>
2026-02-08 13:56:43 +0530

    fix keyvault error

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -8,8 +8,7 @@ resource "azurerm_key_vault" "astralbound-key-vault" {
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = azuread_service_principal.github_oidc_sp.object_id
-
+    object_id = data.azurerm_client_config.current.object_id
     key_permissions = [
       "Get",
       "Create",
