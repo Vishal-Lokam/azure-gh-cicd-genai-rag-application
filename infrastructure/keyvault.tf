117c96a181fa93eea7525b16ef2e68fd5faf83f9
VishalLokam <vishalvikaslokam@gmail.com>
2026-02-08 16:10:07 +0530

    update keyvault access policy

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -5,7 +5,7 @@ resource "azurerm_key_vault" "astralbound_key_vault" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
-  enable_rbac_authorization  = true
+  rbac_authorization_enabled = true
 }
 
 resource "azurerm_key_vault_access_policy" "kv_oidc_access_poicy" {
