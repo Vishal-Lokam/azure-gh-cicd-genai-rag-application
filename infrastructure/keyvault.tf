f6634d5501df513409ae0a4395013bd2fee1826b
VishalLokam <vishalvikaslokam@gmail.com>
2026-02-08 15:13:23 +0530

    update keyvault access policy

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -5,34 +5,9 @@ resource "azurerm_key_vault" "astralbound_key_vault" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-    key_permissions = [
-      "Get",
-      "Create",
-      "Delete",
-      "List"
-    ]
-
-    secret_permissions = [
-      "Get",
-      "Set",
-      "List",
-      "Delete",
-      "Purge",
-      "Recover"
-    ]
-
-    storage_permissions = [
-      "Get",
-      "Set"
-    ]
-  }
 }
 
-resource "azurerm_key_vault_access_policy" "kc_oidc_access_poicy" {
+resource "azurerm_key_vault_access_policy" "kv_oidc_access_poicy" {
   key_vault_id = azurerm_key_vault.astralbound_key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
@@ -59,7 +34,7 @@ resource "azurerm_key_vault_access_policy" "kc_oidc_access_poicy" {
   ]
 }
 
-resource "azurerm_key_vault_access_policy" "kc_oidc_access_poicy" {
+resource "azurerm_key_vault_access_policy" "kv_user_access_poicy" {
   key_vault_id = azurerm_key_vault.astralbound_key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = "33bbf20f-9bb6-4c40-9d22-c42eac9f1d9b"
